211720fede7ce6b6bec29d2cadfe15d9dab23657
Vladimir <gubinvs@gmail.com>
2024-06-24 18:53:10 +0300

    dw

--- EnComponentStore.Core/Abstractions/User.cs
+++ EnComponentStore.Core/Abstractions/User.cs
@@ -6,13 +6,36 @@ namespace EnComponentStore.Core.Abstractions;
     /// </summary>
     /// 
 
-    abstract class User
+    internal abstract class User
     {
+        public string str = string.Empty;
+
+        private User(string name, string login, string password, string email, string position)
+        {
+            this.id = Guid.NewGuid().ToString();
+            this.name = name;
+            this.login = login;
+            this.password = password;
+            this.email = email;
+            this.position = position;
+        }
+
+
+        public (User user, string str) AddUser (string name, string login, string password, string email, string position)
+        {
+            // Прописать проверки на соответствие ,если проверка на соответствие не пройдена, заполнить строку данными.
+            if (name != "d") { str = "kkasjkasjd"; }
+
+            User user = new User(name, login, password, email, position);
+
+            // Возвращает созданного пользователя и состояние строки. если была ошибка строка будет заполнена.
+            return (user, str);
+        }
 
         /// <summary>
         /// Идентификатор GUID пользователя
         /// </summary>
-        string id { get; }
+        string id { get; set; }
 
         /// <summary>
         /// Имя пользователя
@@ -39,5 +62,5 @@ namespace EnComponentStore.Core.Abstractions;
         /// </summary>
         string position { get; }
 
-    }
-}
+    };
+};
