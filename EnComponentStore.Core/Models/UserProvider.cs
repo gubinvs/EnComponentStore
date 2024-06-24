de3a65c57bf3a5141bad7da3128ba200026cda1c
Vladimir <gubinvs@gmail.com>
2024-06-24 19:38:27 +0300

    sf

---
+++ EnComponentStore.Core/Models/UserProvider.cs
@@ -0,0 +1,11 @@
+﻿using EnComponentStore.Core.Abstractions;
+
+namespace EnComponentStore.Core.Models
+{
+    public class UserProvider : User
+    {
+        UserProvider user = new UserProvider("asas", "sds", "sd", "wefwef", "ewe");
+       
+        
+    }
+}
